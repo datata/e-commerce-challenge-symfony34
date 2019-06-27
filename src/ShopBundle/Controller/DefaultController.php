7471e0bda076edf6f72da78a3fbcc04124bd9dcd
datata <danitt15@gmail.com>
2019-06-27 12:57:27 +0200

    Add Users working

--- src/ShopBundle/Controller/DefaultController.php
+++ src/ShopBundle/Controller/DefaultController.php
@@ -5,6 +5,9 @@ namespace ShopBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+use ShopBundle\Entity\User;
+use Symfony\Component\HttpFoundation\Response;
+
 class DefaultController extends Controller
 {
     /**
@@ -13,5 +16,24 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('@Shop/Default/index.html.twig');
+    }
+
+    /**
+     * @Route("/register")
+     */
+    public function addUser()
+    {
+        $user = new User();
+        $user->setName('Ruben');
+        $user->setEmail('Ruben@danitarazona.dev');
+        $user->setCreateAt(new \DateTime('now'));
+        $user->setUpdateAt(new \DateTime('now'));
+
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        return new Response("Usuario creado" . $user->getId());
     }
 }
