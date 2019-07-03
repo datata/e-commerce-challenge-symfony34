d896f4a1e8fa2f1ccd21c1b827bfd2a78327bbde
datata <danitt15@gmail.com>
2019-07-03 16:29:33 +0200

    Crud generated php bin/console doctrine:generate:crud AdminBundle:Product

--- src/AdminBundle/Controller/ProductController.php
+++ src/AdminBundle/Controller/ProductController.php
@@ -2,64 +2,136 @@
 
 namespace AdminBundle\Controller;
 
+use AdminBundle\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-
-use AdminBundle\Entity\Category;
-use AdminBundle\Entity\Brand;
-use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
 
 /**
- * @Route("/product")
+ * Product controller.
+ *
+ * @Route("product")
  */
-
 class ProductController extends Controller
 {
-
     /**
-     * @Route("/")
+     * Lists all product entities.
+     *
+     * @Route("/", name="product_index")
+     * @Method("GET")
      */
     public function indexAction()
     {
-        return $this->render('@Shop/Default/index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+
+        $products = $em->getRepository('AdminBundle:Product')->findAll();
+
+        return $this->render('product/index.html.twig', array(
+            'products' => $products,
+        ));
     }
 
     /**
-     * @Route("/addbrand")
+     * Creates a new product entity.
+     *
+     * @Route("/new", name="product_new")
+     * @Method({"GET", "POST"})
      */
-
-    public function add()
+    public function newAction(Request $request)
     {
-        $brand = new Brand();
-        $brand->setBrand('Adidas');
+        $product = new Product();
+        $form = $this->createForm('AdminBundle\Form\ProductType', $product);
+        $form->handleRequest($request);
 
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($brand);
-        $em->flush();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($product);
+            $em->flush();
+            dump($product->getId());
+            return $this->redirectToRoute('product_show', array('id' => $product->getId()));
+        }
 
-        return new Response("Marca creada " . $brand->getBrand());
+        return $this->render('product/new.html.twig', array(
+            'product' => $product,
+            'form' => $form->createView(),
+        ));
     }
 
-     /**
-     * @Route("/addcategory")
+    /**
+     * Finds and displays a product entity.
+     *
+     * @Route("/{id}", name="product_show")
+     * @Method("GET")
      */
-
-    public function addcategory()
+    public function showAction(Product $product)
     {
-        $em = $this->getDoctrine()->getManager();
+        dump($product);
+        $deleteForm = $this->createDeleteForm($product);
 
-        $cat= $_POST['category'];           
-
-        $category = new Category();
-        $category->model = $cat;
-
-        $em->persist($category);
-        $em->flush();
-
-        return new Response("Modelo creado " . $category->getModel());
+        return $this->render('product/show.html.twig', array(
+            'product' => $product,
+            'delete_form' => $deleteForm->createView(),
+        ));
     }
 
+    /**
+     * Displays a form to edit an existing product entity.
+     *
+     * @Route("/{id}/edit", name="product_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Product $product)
+    {
+        $deleteForm = $this->createDeleteForm($product);
+        $editForm = $this->createForm('AdminBundle\Form\ProductType', $product);
+        $editForm->handleRequest($request);
 
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
 
+            return $this->redirectToRoute('product_edit', array('id' => $product->getId()));
+        }
 
+        return $this->render('product/edit.html.twig', array(
+            'product' => $product,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a product entity.
+     *
+     * @Route("/{id}", name="product_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Product $product)
+    {
+        $form = $this->createDeleteForm($product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($product);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('product_index');
+    }
+
+    /**
+     * Creates a form to delete a product entity.
+     *
+     * @param Product $product The product entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Product $product)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('product_delete', array('id' => $product->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
 }
