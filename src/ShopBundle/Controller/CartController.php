53481c5cb7e3ca41623f40a8d591e47d261fc185
datata <danitt15@gmail.com>
2019-07-09 15:32:09 +0200

    AddAction added into CartController

--- src/ShopBundle/Controller/CartController.php
+++ src/ShopBundle/Controller/CartController.php
@@ -5,17 +5,53 @@ namespace ShopBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+use Symfony\Component\HttpFoundation\Response;
+
+use ShopBundle\Entity\Cart;
+use AdminBundle\AdminBundle;
+
 /**
-     * @Route("/cart", name="index_shop")
-     */
+ * @Route("/cart", name="index_shop")
+ */
 
 class CartController extends Controller
 {
     /**
-     * @Route("/", name="cart_shop")
+     * @Route("/")
      */
     public function indexAction()
-    {
+    {   
         return $this->render('@Shop/Default/cart.html.twig');
     }
+
+    /**
+     * @Route("/addproduct/{id}/{user}", name="addcart")
+     */
+    public function addAction($id,$user)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $cart = $em->getRepository('ShopBundle:Cart');
+
+        $cart = $cart->findOneBy(array('idproducto'=>$id, 'iduser'=>$user));
+               
+        if(!$cart)
+        {
+            $cart = new Cart();
+            $cart->setIdproducto($id);
+            $cart->setIduser($user);
+            $cart->setQuantityproduct(1);
+        }
+        else{
+            $cart->setQuantityproduct($cart->getQuantityproduct()+1);
+        }
+
+        $em->flush();
+        $em->persist($cart);             
+
+        return new Response("Listo!");
+
+    }
+
+
+
 }
